239398414668359d0eb3608a4f7ce3ad321afac3
kaser137 <murilon375@gmail.com>
2023-12-01 02:35:54 +0200

    update

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -91,7 +91,6 @@ def view_restaurants(request):
 
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
-    f=2+'sasha'
     order_items = Order.objects.exclude(status='4')
     for order in order_items:
         if not order.restaurant:
