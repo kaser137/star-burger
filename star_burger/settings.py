d84d6023d0de3145d92bc2ca0619a2a1e94d6bc6
kaser137 <murilon375@gmail.com>
2023-12-22 14:28:32 +0200

    change var name

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 APIKEY = env('YANDEX_KEY')
 ROLLBAR_TOKEN = env('ROLLBAR_TOKEN')
-ENV_NAME = env('ENV_NAME')
+ROLLBAR_ENVIRONMENT_NAME = env('ENV_NAME')
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
 INSTALLED_APPS = [
@@ -64,7 +64,7 @@ DEBUG_TOOLBAR_PANELS = [
 
 ROLLBAR = {
     'access_token': ROLLBAR_TOKEN,
-    'environment': ENV_NAME,
+    'environment': ROLLBAR_ENVIRONMENT_NAME,
     'code_version': '1.0',
     'root': BASE_DIR,
 }
